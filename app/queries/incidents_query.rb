4c9fe024d31ff235428141a8d18fad105bcd7205
ajeffika <jan.mistarz6767@gmail.com>
2019-06-11 12:11:51 +0200

    fixed incidents query

--- app/queries/incidents_query.rb
+++ app/queries/incidents_query.rb
@@ -1,14 +1,11 @@
 # frozen_string_literal: true
 
 class IncidentsQuery
-
   def initialize
     @user_groups = User.first.groups
   end
 
   def fetch
-    @user_groups.each do |z|
-      Incident.joins(:group, :user).where('incidents.group_id = ?', z)
-    end
+    Incident.where(group_id: @user_groups.pluck(:id))
   end
 end
