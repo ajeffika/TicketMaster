bf5bd925fd6ec841eda7bf1613eb25a9914be4bd
ajeffika <jan.mistarz6767@gmail.com>
2019-06-29 15:13:54 +0200

    fixed incident index test

--- spec/controllers/api/v1/incidents_controller_spec.rb
+++ spec/controllers/api/v1/incidents_controller_spec.rb
@@ -4,22 +4,21 @@ require 'rails_helper'
 include AuthorizationHelper
 RSpec.describe Api::V1::IncidentsController, type: :request do
   let(:incident) { FactoryBot.create(:incident) }
-  # let(:user) { FactoryBot.create(:user) }
-
   before :each do
-    test_user = FactoryBot.create(:user)
-
-    binding.pry
-
-    sign_up(test_user)
-    @auth_tokens = auth_tokens_for_user(test_user)
+    @test_user = FactoryBot.create(:user)
+    @auth_tokens = auth_tokens_for_user(@test_user)
   end
-
   describe 'GET #index' do
     it 'returns http success' do
-      binding.pry
       get api_v1_incidents_path, as: :json, headers: @auth_tokens
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET #show' do
+    it 'returns http success' do
+    get api_v1_incident_path(id: 0), as: :json, headers: @auth_tokens
+    expect(response).to have_http_status(:success)
     end
   end
 end
--- spec/support/authorization_helper.rb
+++ spec/support/authorization_helper.rb
@@ -1,18 +1,16 @@
 module AuthorizationHelper
   def sign_up(user)
     # The argument 'user' should be a hash that includes the params 'email' and 'password'.
-            binding.pry
     post user_registration_path,
-         params: { email: user.email,
-                   password: user.password },
+         params: { email: user[:email],
+                   password: user[:password] },
                    #password_confirmation: user[:password] },
          as: :json
   end
 
   def auth_tokens_for_user(user)
     # The argument 'user' should be a hash that includes the params 'email' and 'password'.
-    post user_session_path, params: { email: user[:email], password: user[:password] },
-         as: :json
+    post user_session_path, params: { email: user.email, password: user.password }, as: :json
     response.headers.slice('client', 'access-token', 'uid')
   end
 end
