e2d56bb47b2766eef972e3ddd43aae3fb85d9c46
ajeffika <jan.mistarz6767@gmail.com>
2019-07-05 13:01:09 +0200

    added pundit

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Api::BaseController < ActionController::Base
+  include Pundit
   include DeviseTokenAuth::Concerns::SetUserByToken
   before_action :authenticate_user!
 end
---
+++ app/policies/incident_policy.rb
@@ -0,0 +1,6 @@
+class IncidentPolicy < ApplicationPolicy
+
+  # def create?
+  #   user.admin?
+  # end
+end
