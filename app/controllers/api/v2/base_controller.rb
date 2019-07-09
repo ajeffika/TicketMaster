58941b1dc370964c35eac206fab29278644580ec
ajeffika <jan.mistarz6767@gmail.com>
2019-07-09 21:06:37 +0200

    fixed incident test

--- app/controllers/api/v2/base_controller.rb
+++ app/controllers/api/v2/base_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Api::BaseController < ActionController::Base
+class Api::V2::BaseController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   before_action :authenticate_user!
 end
