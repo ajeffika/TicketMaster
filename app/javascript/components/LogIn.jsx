bad7388cb7639e9dd2c2a43665cda4d1f6c05c20
ajeffika <jan.mistarz6767@gmail.com>
2019-07-26 18:49:50 +0200

    added a empty component

---
+++ app/javascript/components/LogIn.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import IncidentList from "./IncidentList";
+
+
+class LogIn extends Component {
+    constructor() {
+        super();
+        this.state = {incidents: []};
+    }
+}
+
+    export default LogIn;
